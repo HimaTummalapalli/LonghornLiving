47e6932c5d10c7a313b2fa9df28eeb0939f17a94
Heyu Zhou <regina.zhou@utexas.edu>
2025-02-08 20:36:38 -0600

    finishing form

---
+++ longhorn-living/src/pages/FormPage.js
@@ -0,0 +1,213 @@
+// Filename - App.js
+// It contains the Form, its Structure
+// and Basic Form Functionalities
+
+import "./Preference.css";
+import { React, useState } from "react";
+
+function FormPage() {
+  const [formData, setFormData] = useState({
+    cleanliness: "",
+    active: "",
+    study: "",
+    sleep: "",
+    extracurricular: "",
+    smoke: "",
+    pet: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  // const handleSubjectChange = (sub) => {
+  //   setSubjects((prev) => ({
+  //     ...prev,
+  //     [sub]: !prev[sub],
+  //   }));
+  // };
+  const handleReset = () => {
+    // Reset all state variables here
+    setFormData({
+      cleanliness: "",
+      active: "",
+      study: "",
+      sleep: "",
+      extracurricular: "",
+      smoke: "",
+      pet: "",
+      clubs: "",
+      about: "",
+    });
+  };
+
+  return (
+    <div className="FormPage">
+      <h1>Let's Get to Know You!</h1>
+      <fieldset>
+        <form action="#" method="get">
+          <label>What level of cleanliness best describes you?</label>
+          <select
+            name="cleanliness"
+            id="cleanliness"
+            value={formData.cleanliness}
+            onChange={(e) =>
+              setFormData({ ...formData, cleanliness: e.target.value })
+            }
+          >
+            <option value="" disabled>
+              Select your Answer
+            </option>
+            <option value="1">Very Messy</option>
+            <option value="2">Somewhat Messy</option>
+            <option value="3">Moderately Tidy</option>
+            <option value="4">Somewhat Neat</option>
+            <option value="5">Very Neat</option>
+          </select>
+          <label>How active are you?</label>
+          <select
+            name="active"
+            id="active"
+            value={formData.active}
+            onChange={(e) =>
+              setFormData({ ...formData, active: e.target.value })
+            }
+          >
+            <option value="" disabled>
+              Select your Answer
+            </option>
+            <option value="1">Couch Potato</option>
+            <option value="2">Lightly Active</option>
+            <option value="3">Moderately Active</option>
+            <option value="4">Highly Active</option>
+            <option value="5">Very Active</option>
+          </select>
+          <label>How often do you study?</label>
+          <select
+            name="study"
+            id="study"
+            value={formData.study}
+            onChange={(e) =>
+              setFormData({ ...formData, study: e.target.value })
+            }
+          >
+            <option value="" disabled>
+              Select your Answer
+            </option>
+            <option value="1">Rarely</option>
+            <option value="2">Occasionally</option>
+            <option value="3">Regularly</option>
+            <option value="4">Frequently</option>
+            <option value="5">All the time</option>
+          </select>
+          <label>How would you describe your sleeping habits?</label>
+          <select
+            name="sleep"
+            id="sleep"
+            value={formData.sleep}
+            onChange={(e) =>
+              setFormData({ ...formData, sleep: e.target.value })
+            }
+          >
+            <option value="" disabled>
+              Select your Answer
+            </option>
+            <option value="1">Early Riser</option>
+            <option value="2">Somewhat Early</option>
+            <option value="3">Balanced</option>
+            <option value="4">Somewhat Late</option>
+            <option value="5">Night Owl</option>
+          </select>
+          <label>How involved are you in extracurricular activities?</label>
+          <select
+            name="extracurricular"
+            id="extracurricular"
+            value={formData.extracurricular}
+            onChange={(e) =>
+              setFormData({ ...formData, extracurricular: e.target.value })
+            }
+          >
+            <option value="" disabled>
+              Select your Answer
+            </option>
+            <option value="1">Not Involved</option>
+            <option value="2">Somewhat Involved</option>
+            <option value="3">Moderately Involved</option>
+            <option value="4">Highly Involved</option>
+            <option value="5">Very Involved</option>
+          </select>
+          <label>Do you smoke?</label>
+          <select
+            name="smoke"
+            id="smoke"
+            value={formData.smoke}
+            onChange={(e) =>
+              setFormData({ ...formData, smoke: e.target.value })
+            }
+          >
+            <option value="" disabled>
+              Select your Answer
+            </option>
+            <option value="1">Yes</option>
+            <option value="2">No</option>
+          </select>
+          <label>Do you plan to bring a pet?</label>
+          <select
+            name="pet"
+            id="pet"
+            value={formData.pet}
+            onChange={(e) => setFormData({ ...formData, pet: e.target.value })}
+          >
+            <option value="" disabled>
+              Select your Answer
+            </option>
+            <option value="1">Yes</option>
+            <option value="2">No</option>
+          </select>
+
+          <label for="clubs">
+            What clubs, organizations, or extracurricular activities are you
+            involved in?
+          </label>
+          <textarea
+            name="clubs"
+            id="clubs"
+            cols="30"
+            rows="5"
+            onChange={(e) =>
+              setFormData({ ...formData, clubs: e.target.value })
+            }
+            placeholder="Answer here so your ideal roommate can find you!"
+            required
+          ></textarea>
+
+          <label for="about">
+            Introduce your self for a little. What are your hobbies? What movies
+            and mucic do you like? Do you like to cook? Any fun or boring facts
+            about yourself?
+          </label>
+          <textarea
+            name="about"
+            id="about"
+            cols="30"
+            rows="5"
+            onChange={(e) =>
+              setFormData({ ...formData, about: e.target.value })
+            }
+            placeholder="Answer here so your ideal roommate can find you!"
+            required
+          ></textarea>
+          <button type="reset" value="reset" onClick={() => handleReset()}>
+            Reset
+          </button>
+          <button type="submit" value="Submit" onClick={(e) => handleSubmit(e)}>
+            Submit
+          </button>
+        </form>
+      </fieldset>
+    </div>
+  );
+}
+
+export default FormPage;
