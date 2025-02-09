1556571451baf4cb547bbedd3d72bc7935fe96eb
hima tummalapalli <himatummalapalli@himas-MacBook-Pro.local>
2025-02-09 00:55:45 -0600

    Added json file and node server, changed Home.js

--- longhorn-living/src/pages/Home.jsx
+++ longhorn-living/src/pages/Home.jsx
@@ -2,36 +2,48 @@
 import React from "react";
 import Grid from "../components/Grid/Grid";
 import "./pages.css"
+import axios from 'axios'
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [cards, setCards] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const getCardData = async() => {
+      try {
+        const response = await axios.get("http://localhost:3000/users")
+        setCards(response.data)
+      } catch (e) {
+        throw setError(e)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    getCardData()
+  }, [])
+
+  if (loading) {
+    return <p>Loading Data ...</p>
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>
+  }
+  
   return (
   <div className="home_div">
     <Grid
-    cards={[{
-      firstName: "Hima",
-      lastName: "T",
-      housingLoc: "Off Campus",
-      major: "CS",
-      year: "Senior"
-    }, {
-      firstName: "Regina",
-      lastName: "Z",
-      housingLoc: "Off Campus",
-      major: "CS",
-      year: "Senior"
-    }, {
-      firstName: "Bob",
-      lastName: "K",
-      housingLoc: "On Campus",
-      major: "Bio",
-      year: "Sophomore"
-    }, {
-      firstName: "John",
-      lastName: "Done",
-      housingLoc: "On Campus",
-      major: "Finance",
-      year: "Freshman"
-    }]}
+    cards={cards.map((card) => ({
+      firstName: card.firstName,
+      lastName: card.lastName,
+      housingLoc: card.housingLoc,
+      major: card.major,
+      year: card.year,
+      gender: card.gender
+    }))}
     >
     </Grid>
   </div>
